501487e5b70809a7f49af57c4716f3ea1f0cd21b
Joseph Quan <josephquajn@Josephs-MacBook-Air.local>
2025-06-11 15:51:44 -0500

    Fix: bind Flask app to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -56,3 +56,8 @@ def history():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 10000))  # <- this gets the right port from Render
+    app.run(host='0.0.0.0', port=port)         # <- bind to all interfaces
