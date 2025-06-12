b9b338454779d15380cd39be2217e66de7030287
Joseph Quan <josephquajn@Josephs-MacBook-Air.local>
2025-06-12 09:06:29 -0500

    Fix host and port for Render

--- app.py
+++ app.py
@@ -55,9 +55,6 @@ def history():
     return render_template('history.html', workouts=data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
